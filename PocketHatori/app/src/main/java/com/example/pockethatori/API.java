1a92917b629080e8e24193cfc6dac2212f173de7
Piyush9140 <99756558+Piyush9140@users.noreply.github.com>
2023-05-15 23:57:46 +0530

    calculator and api button working

--- PocketHatori/app/src/main/java/com/example/pockethatori/API.java
+++ PocketHatori/app/src/main/java/com/example/pockethatori/API.java
@@ -1,15 +1,13 @@
 package com.example.pockethatori;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class Calculator extends AppCompatActivity {
+public class API extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calculator);
-
+        setContentView(R.layout.activity_api);
     }
 }
--- PocketHatori/app/src/main/java/com/example/pockethatori/Menu.java
+++ PocketHatori/app/src/main/java/com/example/pockethatori/Menu.java
@@ -1,14 +1,40 @@
 package com.example.pockethatori;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
-
+import android.widget.Button;
 public class Menu extends AppCompatActivity {
-
+    Button button;
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        button = (Button) findViewById(R.id.calculator_button);
+        button1 = (Button) findViewById(R.id.API_button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity1();
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public  void openNewActivity1(){
+        Intent intent = new Intent(this,API.class );
+        startActivity(intent);
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, Calculator.class);
+        startActivity(intent);
     }
 }
+
+
